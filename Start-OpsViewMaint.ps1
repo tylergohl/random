a7fccd197b2609f0c22767f3b5f3174bec4fad8c
Tyler Gohl <tjgohl@gmail.com>
2014-10-17 23:25:59 -0500

    Commit

--- Start-OpsViewMaint.ps1
+++ Start-OpsViewMaint.ps1
@@ -1,27 +1,39 @@
 # Start-OpsViewMaint.ps1
-# v0.1
+# v0.2
 # T.J. Gohl
 # tjgohl@gmail.com
-
-
 
 Param (
   
   [parameter(Mandatory=$true, ValueFromPipeline=$true)]
-  [ValidateScript({Resolve-DNSName $_})]
+# [ValidateScript({Resolve-DNSName $_})]
   [String[]]
-  $hostname
+  $Hostname,
+  
+  [parameter(Mandatory=$false)]
+  [Int]
+  $MaintMinutes = 60,
+  
+  [parameter(Mandatory=$false)]
+  [String]
+  $Comment = "Scripted REST Maintenance"
+  
 )
+
+
 
 Begin {
   
   $opsurl = 'https://opsviewurl.company.com/rest'
-  $username = '#####'
+  $username = 'opsviewaccount'
   $login = @{
         'username' = $username
-        'password' = '#####'
-  
+        'password' = 'your_password'
   }
+   
+  $MaintMinutesString = $MaintMinutes -as [string]  
+  $MaintMinutesString = $MaintMinutesString.Insert(0,"+") + "m"
+  
 }
 
 process {
@@ -29,8 +41,18 @@ process {
   foreach ($opsviewhost in $hostname) { 	
 	
 	$token = Invoke-RestMethod -Method Post -Uri "$opsurl/login" -body $login
-    $downtimebody = '{"starttime":"now","endtime":"+1h","comment":"REST Maintenance"}'
-    Invoke-RestMethod -Method Post -Uri "$opsurl/downtime?host=$opsviewhost" -Headers @{"X-Opsview-Username"="$username";"X-Opsview-Token"="$($token.token)"} -body $downtimebody -contentType "application/json"  
-  
+	$downtimebody = @{
+	  starttime = "now"
+	  endtime = $MaintMinutesString
+	  comment = $Comment
+    }
+
+	$downtimebody = $downtimebody | convertto-json -compress
+
+    $result = Invoke-RestMethod -Method Post -Uri "$opsurl/downtime?host=$opsviewhost" -Headers @{"X-Opsview-Username"="$username";"X-Opsview-Token"="$($token.token)"} -body $downtimebody -contentType "application/json"  
+    
+    if ($result.summary.num_hosts -gt 0) {
+      write-host "$($result.list.hosts.hostname) successfully placed into maintenance mode for $MaintMinutes minute(s)!" -foregroundcolor "green"
+    }
   }
 }
