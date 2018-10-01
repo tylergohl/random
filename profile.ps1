24a6f92ab9003a85feb7de37219dd0609f78ce0d
T.J. Gohl <tjgohl@gmail.com>
2018-10-01 10:14:15 -0400

    init profile

---
+++ profile.ps1
@@ -0,0 +1,21 @@
+# Throw in $Profile.AllUsersAllHosts
+
+
+Import-Module posh-git
+Import-Module AWSPowerShell.NetCore
+Set-DefaultAWSRegion -Region us-east-1
+
+function GetEC2InstanceByName($InstanceNameFilter) {
+    $InstancesObject = @()
+    $Instances = (Get-EC2Instance -Filter @( @{name='tag:Name'; values="$InstanceNameFilter"})).Instances
+    
+    ForEach ($Instance in $Instances) {    
+        $InstanceName = $Instance.tags | where-object key -eq "Name" | Select-Object Value -expand Value
+    
+        $InstancesObject += [pscustomobject][ordered]@{
+            InstanceId = $Instance.InstanceId
+            InstanceName = $InstanceName
+        }
+    }
+    return $InstancesObject | sort-object InstanceName
+}
